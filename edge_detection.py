51afa4441af289f44536934e70c1b386e3673180
RishabhBhatnagar <bhatnagarrishabh4@gmail.com>
2019-10-27 23:11:59 +0530

    Add Non-Pep8 EdgeDetection

---
+++ edge_detection.py
@@ -0,0 +1,31 @@
+def nabla(img, k, j):
+    '''
+    |(k  , j)    (k,    j + 1)|
+    |(k+1, j)    (k + 1, j + 1)|
+    '''
+    i1 = (abs(I[k + 1][j] - I[k][j]) + abs(I[k + 1][j + 1] - I[k][j + 1])) >> 2  # horizontal direction
+    i2 = (abs(I[k][j + 1] - I[k][j]) + abs(I[k + 1][j + 1] - I[k + 1][j])) >> 2  # vertical direction
+    return (pow(i1, 2) + pow(i2, 2)) ** 0.5
+
+
+def edge_detection(img, epsilon):
+    m = len(img)
+    n = len(img[0])
+    edge_image = [[0] * n] * m
+    for k in range(m - 1):
+        for l in range(n - 1):
+            if nabla(img, k, j) >= epsilon:
+                edge_image[k][j] = 1
+    return edge_image
+
+
+if __name__ == "__main__":
+    img = [
+        [0, 0, 5, 0],
+        [0, 0, 4, 0],
+        [0, 0, 3, 0],
+        [0, 0, 4, 0],
+        [0, 0, 0, 0]
+    ]
+    edges = edge_detection(img, epsilon=2)
+
