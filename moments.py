fdcd1af14c2233309429bebde3da6b6ce1763c07
RishabhBhatnagar <bhatnagarrishabh4@gmail.com>
2019-10-28 18:39:08 +0530

    Modified for One Based Indexing

--- moments.py
+++ moments.py
@@ -1,10 +1,10 @@
 def moments(image):
     mu_00 = mu_10 = mu_01 = mu_11 = 0
-    for x in range(len(image)):
-        for y in range(len(image[0])):
-            mu_00 += image[i][j]
-            mu_10 += image[i][j] * x
-            mu_01 += image[i][j] * y
-            mu_11 += image[i][j] * x * y
+    for x in range(1, len(image) + 1):
+        for y in range(1, len(image[0]) + 1):
+            mu_00 += image[x - 1][y - 1]
+            mu_10 += image[x - 1][y - 1] * x
+            mu_01 += image[x - 1][y - 1] * y
+            mu_11 += image[x - 1][y - 1] * x * y
     return [mu_00, mu_01, mu_10, mu_11]
 
